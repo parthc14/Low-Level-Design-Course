66c9d22dd8784360930937220d756f9b4a661819
Parth Chitroda <parthchitroda@Parths-MacBook-Air.local>
2026-04-18 19:52:23 -0700

    feat: Implementation of Singleton in Python

---
+++ Lecture 10/Python/simple_singleton.py
@@ -0,0 +1,14 @@
+class Singleton:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
+    print(f"Are they same: {s1 == s2}")
+
+        
