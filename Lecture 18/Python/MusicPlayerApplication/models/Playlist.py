c92a24e92fdbfd90fdda91ae2c123a1d580ec520
Parth Chitroda <parthchitroda@Parths-MacBook-Air.local>
2026-04-27 21:22:01 -0700

    fix: Update Playlist model

--- Lecture 18/Python/MusicPlayerApplication/models/Playlist.py
+++ Lecture 18/Python/MusicPlayerApplication/models/Playlist.py
@@ -2,24 +2,23 @@ from typing import List
 from Song import Song
 
 class Playlist:
-    song_list: List[Song] = []
 
     def __init__(self, name: str):
         self.__play_list_name = name
+        self.__song_list:  List[Song] = []
 
     def get_playlist_name(self):
         return self.__play_list_name
+
+    def get_songslist(self):
+        return self.__song_list
     
     @classmethod
-    def get_songslist():
-        return Playlist.song_list
-    
-    @classmethod
-    def get_number_of_songs():
-        return len(Playlist.song_list)
+    def get_number_of_songs(self):
+        return len(self.get_songslist())
     
 
     def add_song_to_playlist(self, song: Song):
         if song is None:
             return RuntimeError("Cannot add null song to playlist")
-        Playlist.song_list.append(song)
+        self.get_songslist().append(song)
