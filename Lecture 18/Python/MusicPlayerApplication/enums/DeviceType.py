349fa8be8a977fa158e33c8e1e9e44c847a3a46b
Parth Chitroda <parthchitroda@Parths-MacBook-Air.local>
2026-04-28 22:01:59 -0700

    feat: Enums and Factory in Python

---
+++ Lecture 18/Python/MusicPlayerApplication/enums/DeviceType.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class DeviceType(Enum):
+    BLUETOOTH : 1
+    WIRED: 2
+    HEADPHONES: 3
---
+++ Lecture 18/Python/MusicPlayerApplication/enums/PlayStrategy.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class PlayStrategyType(Enum):
+    SEQUENTIAL = 1
+    RANDOM = 2
+    CUSTOM_QUEUE = 3
